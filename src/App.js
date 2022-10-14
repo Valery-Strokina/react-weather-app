d217764b8592f368e5bc13b483c6eb4c319639a7
Valery-Strokina <ms.nanyeva@gmail.com>
2022-10-14 14:44:58 +0300

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://stunning-macaron-7fb77f.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Valeriia Strokina{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Valery-Strokina/Valery-Weather-App.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
