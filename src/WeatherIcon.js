821882a0c9af76e316f67ca72a9efd0f7fe1cb29
Valery-Strokina <ms.nanyeva@gmail.com>
2022-10-15 17:42:30 +0300

    display forecast of 1 day with fake data

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#7c7c7c"
-      size={52}
+      size={props.size}
       animate={true}
     />
   );
